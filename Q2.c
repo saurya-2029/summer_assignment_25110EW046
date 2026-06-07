a7858ebdba8a7c4fdfe800c2d298524363cb38ce
saurya-2029 <sauryag28@gmail.com>
2026-06-07 14:18:58 +0530

    day-7 uploaded

---
+++ Q2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int fib(int n)
+{
+    if (n==0)
+    return 0;
+    else if (n==1)
+    return 1;
+    else
+    return fib(n-1) + fib(n-2);
+}
+int main()
+{
+    int n, i;
+    printf("enter a number: ");
+    scanf("%d", &n);
+    printf("fibonacci of series is: ");
+    for(i=0; i<n; i++)
+    {
+        printf("%d ", fib(i));
+    }
+    return 0;
+}
