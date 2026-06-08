82b858946d843060d0018a30c1facf12e9bdf970
saurya-2029 <sauryag28@gmail.com>
2026-06-08 12:34:01 +0530

    day-8 uploaded

---
+++ Q1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, j, n;
+    cout<<"enter the number of rows: ";
+    cin>>n;
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=i; j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
