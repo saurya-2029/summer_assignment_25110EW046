8e2e1f3ad820504514311fd2e7fef7926d6f597d
saurya-2029 <sauryag28@gmail.com>
2026-06-10 13:14:27 +0530

    day-10 uploaded

---
+++ Q1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, j, space;
+    for(i=1; i<=5; i++)
+    {
+        for(space=1; space<=5-i; space++)
+        {
+            cout<<" ";
+        }
+        {
+            for(j=1; j<=2*i-1; j++)
+            {
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+    }
+}
